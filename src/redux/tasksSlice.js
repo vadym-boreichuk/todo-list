96ffea9e50c752e99cedfedf3be9b8d804c84467
Vadym Boreichuk <109723184+vadym-boreichuk@users.noreply.github.com>
2023-06-22 08:30:05 +0200

    test

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,20 +1,22 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-const initialState = [
-  { id: 0, text: "Learn HTML and CSS", completed: true },
-  { id: 1, text: "Get good at JavaScript", completed: true },
-  { id: 2, text: "Master React", completed: false },
-  { id: 3, text: "Discover Redux", completed: false },
-  { id: 4, text: "Build amazing apps", completed: false },
-];
+const initialState = {
+  tasks: [
+    { id: 0, text: "Learn HTML and CSS", completed: true },
+    { id: 1, text: "Get good at JavaScript", completed: true },
+    { id: 2, text: "Master React", completed: false },
+    { id: 3, text: "Discover Redux", completed: false },
+    { id: 4, text: "Build amazing apps", completed: false },
+  ],
+};
 
 const tasksSlice = createSlice({
   name: "tasks",
-  initialState: initialState,
+  initialState: initialState.tasks,
   reducers: {
     addTask: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.tasks.push(action.payload);
       },
       prepare(text) {
         return {
@@ -27,11 +29,11 @@ const tasksSlice = createSlice({
       },
     },
     deleteTask(state, action) {
-      const index = state.findIndex((task) => task.id === action.payload);
-      state.splice(index, 1);
+      const index = state.tasks.findIndex((task) => task.id === action.payload);
+      state.tasks.splice(index, 1);
     },
     toggleCompleted(state, action) {
-      for (const task of state) {
+      for (const task of state.tasks) {
         if (task.id === action.payload) {
           task.completed = !task.completed;
           break;
